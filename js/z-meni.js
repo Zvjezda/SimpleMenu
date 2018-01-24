e44cb8e76cd34a667c8fbf2658c1140cbcd3984b
Zvjezda <zvjezdana_knezevic2000@gmail.com>
2018-01-24 12:35:16 +0100

    menu update

---
+++ js/z-meni.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var meniHeight = $(window).height();
+    $('div[name="z-meni"]').css('height', meniHeight);
+	
+	$(".closeMeny").on("click", function(){
+		$(this).hide("slide", { direction: "left" }, 1000);
+		
+	});
+});
