6023c525babb5720b26804400e071c3aca5a0ba9
Zvjezda <zvjezdana_knezevic2000@gmail.com>
2018-01-24 13:05:13 +0100

    small fix

--- js/z-meni.js
+++ js/z-meni.js
@@ -1,9 +1,4 @@
 ﻿$(document).ready(function () {
     var meniHeight = $(window).height();
     $('div[name="z-meni"]').css('height', meniHeight);
-	
-	$(".closeMeny").on("click", function(){
-		$(this).hide("slide", { direction: "left" }, 1000);
-		
-	});
 });
